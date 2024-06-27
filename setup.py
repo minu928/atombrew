93cbef307726382ac1746cbd200776f3c5b60114
MyKnu <k1alty@naver.com>
2024-06-27 22:46:42 +0900

    update 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 
 version_dict = {}
-with open(Path(__file__).parents[0] / "atombrew/__version__.py") as this_v:
+with open(Path(__file__).parents[0] / "atombrew/__version.py") as this_v:
     exec(this_v.read(), version_dict)
 version = version_dict["__version__"]
 del version_dict
