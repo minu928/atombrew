b8462f26394ee9c3ce51ad854afcdd45c8cbb734
MyKnu <k1alty@naver.com>
2024-06-27 22:49:56 +0900

    update 0.0.2

--- atombrew/__init__.py
+++ atombrew/__init__.py
@@ -1,5 +1,5 @@
 from .__version import __version__
-from . import io
+from . import io, atom
 from ._home import Home
 
-__all__ = ["__version", "io", "Home"]
+__all__ = ["__version", "Home", "io", "atom"]
